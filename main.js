b9cf6ea277c727a31e9597a17e9551abb13d023f
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-16 15:33:37 +0330

    Keyup

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const charectorElement = document.querySelector(".charecter");
 const threeElement = document.querySelector(".block");
+
+document.addEventListener('keyup',(e)=>{
+if(e.key === ' '){
+charectorElement.classList.add("jupmAnimate");
+}
+});

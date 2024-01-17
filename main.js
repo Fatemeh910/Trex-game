efa8fdf8152de1eeb9ea2778b9276a755ba6e10f
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-17 19:31:10 +0330

    Fasele trex az bala va derakht az chap

--- main.js
+++ main.js
@@ -17,9 +17,10 @@ document.addEventListener("keyup", (e) => {
   const checkCondition = () =>{
     const trexTop = parseInt(window.getComputedStyle(trexElement).top);
     const treeLeft = parseInt(window.getComputedStyle(treeElement).left);
-    
-    if(treeLeft < 90 && trexTop >= 230){
 
+    if(treeLeft < 90 && trexTop >= 230){
+    trexElement.style.top = `$(trexTop)px`;
+    treeElement.style.display = 'none';
     }
   };
   setInterval(() => {

9b971faa48c165865437fb77945d59cf2a10bf3d
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-17 20:52:03 +0330

    coment gozari

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
-///ELEMENTS
+// ELEMENTS
 const trexElement = document.querySelector(".charecter");
 const treeElement = document.querySelector(".block");
 
-//REASTART PAGE ELEMENTs
-const restartPage = document.querySelector('.restart-page');
-const restartButton = document.querySelector('.restart-btn');
+// RESTART PAGE ELEMENTS
+const restartPage = document.querySelector(".restart-page");
+const restartButton = document.querySelector(".restart-btn");
 
-///JUMP TREX CHARECTER WHEN WE CLICK SPAse BUTTON
+// JUMP T-REX CHARACTER WHEN WE CLICK SPACE BUTTON
 document.addEventListener("keyup", (e) => {
-    if (e.key === " ") {
-      trexElement.classList.add("jumpAnimate");
-  
-      setTimeout(() => {
-        trexElement.classList.remove("jumpAnimate");
-      }, 500);
-    }
-  });
+  if (e.key === " ") {
+    trexElement.classList.add("jumpAnimate");
 
-  //چک کن ایا ماباختیم یانه
-  const checkCondition = () => {
-    const trexTop = parseInt(window.getComputedStyle(trexElement).top);
-    const treeLeft = parseInt(window.getComputedStyle(treeElement).left);
-  
-    if (treeLeft < 90 && trexTop >= 230) {
-      trexElement.style.top = `${trexTop}px`;
-      treeElement.style.display = "none";
-      restartPage.classList.add("show-restart-page");
-      clearInterval(tirexInterval);
-    }
-  };
-//فانکشن چک کالدیز رو هرده ثانیه فراخوانی کن
+    setTimeout(() => {
+      trexElement.classList.remove("jumpAnimate");
+    }, 500);
+  }
+});
+
+// CHECK IF WE LOSE OR NOT
+//چگ کن باختیم یانه
+const checkCondition = () => {
+  const trexTop = parseInt(window.getComputedStyle(trexElement).top);
+  const treeLeft = parseInt(window.getComputedStyle(treeElement).left);
+
+  if (treeLeft < 90 && trexTop >= 230) {
+    trexElement.style.top = `${trexTop}px`;
+    treeElement.style.display = "none";
+    restartPage.classList.add("show-restart-page");
+    clearInterval(tirexInterval);
+  }
+};
+
+// CALLS CHECKCONDITION FUNCTOIN EVERY 10MS
+//فانکشن چک کوندینزرو هرده ثانیه فراخوانی کن
 const tirexInterval = setInterval(checkCondition, 10);
 
-  ///بازس ری استارت شودزمانی که رودکمه ری استارت کلیک کنیم
-  restartButton.addEventListener("click", () => {
-    restartPage.classList.remove("show-restart-page");
-    trexElement.style.top = "";
-    treeElement.style.display = "block";
-    setInterval(checkCondition, 10);
-  });
+// RESTART GAME WHEN WE CLICK RESTART BUTTON
+//بازی ری استارت شودزمانی که روی دکمه ری استارت کلیک کردیم
+restartButton.addEventListener("click", () => {
+  restartPage.classList.remove("show-restart-page");
+  trexElement.style.top = "";
+  treeElement.style.display = "block";
+  setInterval(checkCondition, 10);
+});

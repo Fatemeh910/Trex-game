29659dc2406716dbfc38ed1a900c48fcce2746e7
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-17 20:45:18 +0330

    Takmil kod

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const trexElement = document.querySelector(".charecter");
 const treeElement = document.querySelector(".block");
 
 //REASTART PAGE ELEMENTs
-const restarPage = document.querySelector('.restart-page');
+const restartPage = document.querySelector('.restart-page');
 const restarButton = document.querySelector('.restart-btn');
 
 ///JUMP TREX CHARECTER WHEN WE CLICK SPAse BUTTON
@@ -18,16 +18,24 @@ document.addEventListener("keyup", (e) => {
   });
 
   //چک کن ایا ماباختیم یانه
-  const checkCondition = () =>{
+  const checkCondition = () => {
     const trexTop = parseInt(window.getComputedStyle(trexElement).top);
     const treeLeft = parseInt(window.getComputedStyle(treeElement).left);
-
-    if(treeLeft < 90 && trexTop >= 230){
-        trexElement.style.top = `$(trexTop)px`;
-        treeElement.style.display = 'none';
-        restarPage.classList.add('show-restart-page');
-        clearInterval(trexInterval);
+  
+    if (treeLeft < 90 && trexTop >= 230) {
+      trexElement.style.top = `${trexTop}px`;
+      treeElement.style.display = "none";
+      restartPage.classList.add("show-restart-page");
+      clearInterval(tirexInterval);
     }
   };
 //فانکشن چک کالدیز رو هرده ثانیه فراخوانی کن
-  const trexInterval =  setInterval(checkCondition, 10);
+const tirexInterval = setInterval(checkCondition, 10);
+
+  ///بازس ری استارت شودزمانی که رودکمه ری استارت کلیک کنیم
+  restartButton.addEventListener("click", () => {
+    restartPage.classList.remove("show-restart-page");
+    trexElement.style.top = "";
+    treeElement.style.display = "block";
+    setInterval(checkCondition, 10);
+  });

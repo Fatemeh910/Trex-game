e149f9e00db66623d6086a1ed9520568d60bd222
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-17 19:43:59 +0330

    derakh napadid shavad

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 const trexElement = document.querySelector(".charecter");
 const treeElement = document.querySelector(".block");
 
+//REASTART PAGE ELEMENTs
+const restarPage = document.querySelector('.restart-page');
+const restarButton = document.querySelector('.restart-btn');
+
 ///JUMP TREX CHARECTER WHEN WE CLICK SPAse BUTTON
 document.addEventListener("keyup", (e) => {
     if (e.key === " ") {
@@ -21,8 +25,7 @@ document.addEventListener("keyup", (e) => {
     if(treeLeft < 90 && trexTop >= 230){
     trexElement.style.top = `$(trexTop)px`;
     treeElement.style.display = 'none';
+    restarPage.classList.add('show-restart-page');
     }
   };
-  setInterval(() => {
-    
-  }, interval);
+  setInterval(checkCondition, 10);
